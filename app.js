d7a24049a1f62281c7f99c9dfaa369a626b66b37
Audy wallace Siegle <audywallace@Audys-MacBook-Pro.local>
2024-07-24 09:40:48 +0800

    add routers

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const router = require('./routers/index');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use('/', router);
 
 const server = app.listen(port, () => {
     console.log(`listening on port ${port}`);
